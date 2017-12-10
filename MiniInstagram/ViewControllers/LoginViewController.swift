8ad0a6c8308bb77ba733c8fa7ed23bbc86b7a98a
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-10 04:44:21 -0800

    added outlets + some auth set up

--- MiniInstagram/ViewControllers/LoginViewController.swift
+++ MiniInstagram/ViewControllers/LoginViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import OAuthSwift
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: OAuthViewController {
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    lazy var internalWebViewController: WebViewController = {
+        let controller = WebViewController()
+        controller.view = UIView(frame: UIScreen.main.bounds)
+        controller.delegate = self
+        controller.viewDidLoad() // allow WebViewController to use this ViewController as parent to be presented
+        return controller
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: private methods
+    // MARK: handler
+    func getURLHandler() -> OAuthSwiftURLHandlerType {
+        if internalWebViewController.parent == nil {
+            self.addChildViewController(internalWebViewController)
+        }
+        return internalWebViewController
 }
+
+    @IBAction func loginButtonClicked(_ sender: Any) {
+    }
+}
+
+extension LoginViewController: OAuthWebViewControllerDelegate {
+    
+    func oauthWebViewControllerDidPresent() {}
+    func oauthWebViewControllerDidDismiss() {}
+    func oauthWebViewControllerWillAppear() {}
+    func oauthWebViewControllerDidAppear() {}
+    func oauthWebViewControllerWillDisappear() {}
+    func oauthWebViewControllerDidDisappear() {
+        // Ensure all listeners are removed if presented web view close
+//        oauthswift?.cancel()
+    }
+}
+
 
