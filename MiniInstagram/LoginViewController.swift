db8e6efd22b04114f6bee7699f1983611bfd423c
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-10 03:25:26 -0800

    Added loginVC

--- MiniInstagram/LoginViewController.swift
+++ MiniInstagram/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MiniInstagram
 //
 //  Created by Renu Punjabi on 12/10/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
