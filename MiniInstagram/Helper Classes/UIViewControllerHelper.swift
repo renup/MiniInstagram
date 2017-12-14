e7d984c3cbb0724747032738a0b4bab954ab83b0
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-13 16:06:53 -0800

    storing keychain + added media coordinator and set it up
    added uiviewcontroller helper class + constants file

---
+++ MiniInstagram/Helper Classes/UIViewControllerHelper.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewControllerHelper.swift
+//  FoodDelivery
+//
+//  Created by Renu Punjabi on 11/3/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+//UIViewController convenience class to instantiate ViewController from storyboard
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    class func defaultNibName() -> String {
+        return String(describing: self)
+    }
+    
+    class func instantiateUsingDefaultStoryboardIdWithStoryboardName(name: String) -> UIViewController {
+        print("defaultname: \(defaultNibName())")
+        return instantiateControllerFromStoryboard(name: name, identifier: defaultNibName())
+    }
+    
+    class func instantiateControllerFromStoryboard(name: String, identifier: String) -> UIViewController {
+        let storyboard = UIStoryboard.init(name: name, bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: identifier)
+    }
+    
+}
