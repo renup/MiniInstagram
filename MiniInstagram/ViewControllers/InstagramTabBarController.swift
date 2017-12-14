0222746ac599986dd8d08ad86805205b190bd7a6
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-14 13:19:48 -0800

    added instagram tabbarcontroller class

---
+++ MiniInstagram/ViewControllers/InstagramTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  InstagramTabBarController.swift
+//  MiniInstagram
+//
+//  Created by Renu Punjabi on 12/14/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InstagramTabBarController: UITabBarController {
+    
+}
