a4929f1a0b1ee632fe5b97969c5e6302a419815e
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-14 14:41:33 -0800

    added apiprocessor class and deleted media storyboard

---
+++ MiniInstagram/Model Files/APIProcessor.swift
@@ -0,0 +1,14 @@
+//
+//  APIProcessor.swift
+//  MiniInstagram
+//
+//  Created by Renu Punjabi on 12/14/17.
+//  Copyright © 2017 Renu Punjabi. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIProcessor: NSObject {
+    
+}
