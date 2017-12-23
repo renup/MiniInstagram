84522dc5fc42eefbf94f9406a2c7bf661389985f
Renu Punjabi <rpunjabi12@gmail.com>
2017-12-23 05:29:17 -0800

    resolved the pods issue and debug file not found issue

--- Podfile
+++ Podfile
@@ -1,24 +1,21 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
 
 target 'MiniInstagram' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
 
 	pod 'SwiftyJSON'
 	pod 'OAuthSwift'
 	pod 'Alamofire'
 	pod 'AlamofireImage'
-    	pod 'KeychainSwift'
-    	pod 'MBProgressHUD', '~> 1.0.0'
+    pod 'KeychainSwift'
+    pod 'MBProgressHUD', '~> 1.0.0'
 
   target 'MiniInstagramTests' do
     inherit! :search_paths
 	    pod 'Quick'
     	pod 'Nimble'
-    	pod 'Mockingjay'
-	pod 'SwiftyJSON'
-    	pod 'KeychainSwift'
+    	pod 'Mockingjay' 
 
    end
 
--- Pods/Target Support Files/Pods-MiniInstagramTests/Pods-MiniInstagramTests-frameworks.sh
+++ Pods/Target Support Files/Pods-MiniInstagramTests/Pods-MiniInstagramTests-frameworks.sh
@@ -102,16 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mockingjay/Mockingjay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/URITemplate/URITemplate.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Mockingjay/Mockingjay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
